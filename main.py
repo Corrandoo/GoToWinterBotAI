327f2c4e8c560242bc5c906a6bb26cbec7cd8057
Corrandoo <ivan260302@yandex.ru>
2017-01-05 12:49:03 +0300

    Added new checkers

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
+import random
+#23OQQVCJ my key
+#field[0][0] = левый верхний угол
+#field[x_size - 1][0] = правый верхний угол
+#field[0][y_size - 1] = левый нижний угол
+#field[x_size - 1][y_size - 1] = правый нижний угол
 
 def make_choice(x,y,field):
     x_size = len(field)
     y_size = len(field[0])
+    isAttackRight = False #ЕСТЬ ЛИ АТАКА СПРАВА
+    isAttackLeft = False #ЕСТЬ ЛИ АТАКА СЛЕВА
+    isAttackUp = False #ЕСТЬ ЛИ АТАКА СВЕРХУ
+    isAttackDown = False #ЕСТЬ ЛИ АТАКА СНИЗУ
+    isUNastyHarder = False #СИЛЬНЕЕ ЛИ ВЕРХНИЙ ПРОТИВНИК
+    isDNastyHarder = False #СИЛЬНЕЕ ЛИ НИЖНИЙ ПРОТИВНИК
+    isLNastyHarder = False #СИЛЬНЕЕ ЛИ ЛЕВЫЙ ПРОТИВНИК
+    isRNastyHarder = False #СИЛЬНЕЕ ЛИ ПРАВЫЙ ПРОТИВНИК
+
+    for i in range(0, x - 1):
+        if field[i][y] != 0 and field[i][y]['history'] == "fire_right":
+            isAttackLeft = True
+    for i in range(x + 1, x_size - 1):
+        if field[i][y] != 0 and field[i][y]['history'] == "fire_left":
+            isAttackRight = True
+    for i in range(0, y - 1):
+        if field[x][i] != 0 and field[x][i]['history'] == "fire_down":
+            isAttackUp = True
+    for i in range(y + 1, y_size - 1):
+        if field[x][i] != 0 and field[x][i]['history'] == "fire_up":
+            isAttackDown = True
+
+
+    for i in range(0, x - 1):
+        if field[i][y] != 0:
+            return "fire_left"
+    for i in range(x + 1, x_size - 1):
+        if field[i][y] != 0:
+            return "fire_right"
+    for i in range(0, y - 1):
+        if field[x][i] != 0:
+            return "fire_up"
+    for i in range(y + 1, y_size - 1):
+        if field[x][i] != 0:
+            return "fire_down"
+
+    return random.choice(["go_left", "go_down"])
